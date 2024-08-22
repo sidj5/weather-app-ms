74c981244a5ee55be85d4143efc4829554924f06
Siddhant Jain <sidj5297@gmail.com>
2024-08-22 12:25:47 +0200

    updated host_timing script

--- callback_plugins/host_timing.py
+++ callback_plugins/host_timing.py
@@ -8,25 +8,29 @@ class CallbackModule(CallbackBase):
 
     def __init__(self, *args, **kwargs):
         super(CallbackModule, self).__init__(*args, **kwargs)
-        self.host_times = {}
+        self.host_start_times = {}
+        self.host_end_times = {}
 
     def v2_playbook_on_start(self, playbook):
-        self.start_time = time.time()
+        pass  # No need to track the start of the entire playbook here
 
     def v2_playbook_on_play_start(self, play):
-        # Initialize host timing at the start of the play
         for host in play.get_variable_manager()._inventory.get_hosts():
-            self.host_times[host.get_name()] = time.time()
+            self.host_start_times[host.get_name()] = None
+
+    def v2_runner_on_start(self, host, task):
+        if self.host_start_times[host.get_name()] is None:
+            self.host_start_times[host.get_name()] = time.time()
 
     def v2_runner_on_ok(self, result):
         host = result._host.get_name()
-        if host not in self.host_times:
-            self.host_times[host] = time.time()
+        self.host_end_times[host] = time.time()
 
     def v2_playbook_on_stats(self, stats):
-        total_time = time.time() - self.start_time
         self._display.banner("HOST-LEVEL TIMING")
-        for host, start_time in self.host_times.items():
-            elapsed_time = time.time() - start_time
-            self._display.display(f"Host {host} took {elapsed_time:.2f} seconds to execute the playbook")
-        self._display.display(f"Total playbook execution time: {total_time:.2f} seconds")
+        for host, start_time in self.host_start_times.items():
+            if start_time:
+                elapsed_time = self.host_end_times[host] - start_time
+                self._display.display(f"Host {host} took {elapsed_time:.2f} seconds to execute the playbook")
+            else:
+                self._display.display(f"Host {host} did not start any tasks")
